ff97074085bfa34ff1c42947f8eaf54e7e59b1c1
Daniel <danielfleck@live.ca>
2019-09-24 19:53:17 -0300

    make forecast icon for today become dynamic

--- src/main/resources/static/js/bitfinexTickerProvider.js
+++ src/main/resources/static/js/bitfinexTickerProvider.js
@@ -20,8 +20,6 @@
 
     function responseTicker(data) {
 
-        console.log(" DATA-> " + data);
-
         var undefined = data[1][5];
 
         var LAST_PRICE = data[1][6];
@@ -34,11 +32,11 @@
             if (splitString[1].charAt(0) != 0){
                 const formattedValue = "-" + splitString[1].charAt(0) + splitString[1].charAt(1) + "," + splitString[1].slice(2,splitString[1].length) + "%";
                 document.getElementById("dailyChangePercentage").innerHTML = formattedValue;
-                window.PercentageToday = formattedValue;
+                updateImages(formattedValue);
             } else {
                 const formattedValue = "-" + splitString[1].charAt(1) + "," + splitString[1].slice(2,splitString[1].length) + "%";
                 document.getElementById("dailyChangePercentage").innerHTML = formattedValue;
-                window.PercentageToday = formattedValue;
+                updateImages(formattedValue);
             }
 
         } else {
@@ -46,19 +44,42 @@
                 const formattedValue = splitString[1].charAt(0) + splitString[1].charAt(1) + "," + splitString[1].slice(2,splitString[1].length) + "%";
                 document.getElementById("dailyChangePercentage").innerHTML = formattedValue;
                 window.PercentageToday = formattedValue;
+                updateImages(formattedValue);
             } else {
                 const formattedValue = splitString[1].charAt(1) + "," + splitString[1].slice(2,splitString[1].length) + "%";
                 document.getElementById("dailyChangePercentage").innerHTML = formattedValue;
                 window.PercentageToday = formattedValue;
+                updateImages(formattedValue);
             }
         }
-
-        var HIGH = data[1][8];
-        var LOW = data[1][9];
-
-
+        // var HIGH = data[1][8];
+        // var LOW = data[1][9];
     }
 
+    function updateImages(formattedValue) {
 
+        var dailyChange = parseFloat(formattedValue.replace(";","."));
+        var forecastIconToday = document.getElementById("forecastIconToday");
+        var imageToday = forecastIconToday.getAttribute("src");
+
+        if (dailyChange > 0 && dailyChange < 1){
+            imageToday = "images/icons/icon-3.svg";
+        } else if ( dailyChange > 0 && dailyChange < 0.5 ) {
+            imageToday = "images/icons/icon-3.svg";
+        } else if ( dailyChange < 0 && dailyChange > -0.5 ) {
+            imageToday = "images/icons/icon-6.svg";
+        } else if ( dailyChange > 1 ) {
+            imageToday = "images/icons/icon-2.svg";
+        } else if (dailyChange < 0 && dailyChange > -1){
+            imageToday = "images/icons/icon-9.svg";
+        } else if (dailyChange < 1 && dailyChange > -2){
+            imageToday = "images/icons/icon-10.svg";
+        } else if (dailyChange < -2 && dailyChange > -7){
+            imageToday = "images/icons/icon-11.svg";
+        } else if (dailyChange < -7){
+            imageToday = "images/icons/icon-8.svg";
+        }
+        forecastIconToday.setAttribute("src", imageToday);
+    }
 
 })();
